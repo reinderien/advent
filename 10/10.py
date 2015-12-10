d585645d9f4924d712f43faba7f2e9d128ffd990
reinderien <gtoombs@lakeheadu.ca>
2015-12-10 08:03:04 -0500

    Solution for 10

--- 10/10.py
+++ 10/10.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python3
 
-inputs = '3113322113'
+def p(N):
+    inputs = '3113322113'
+    for n in range(N):
+        prev, out, run = inputs[0], '', 1
+        for i in inputs[1:]:
+            if i == prev: run += 1
+            else:
+                out += '%d%c' % (run, prev)
+                run, prev = 1, i
+        inputs = '%s%d%c' % (out, run, prev)
+    print(len(inputs))
+
+p(40)  # 329356
+p(50)  # 4666278
