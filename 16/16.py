4d6501af627e40ae346d80667a7d14fbe4091ed9
reinderien <gtoombs@lakeheadu.ca>
2015-12-17 15:27:29 -0500

    16 done

--- 16/16.py
+++ 16/16.py
@@ -9,8 +9,25 @@ def p1():
     sues = ({m.groups()[:2]
              for m in re.finditer(r' ([a-z]+): (\d+)(,|$)', line)}
             for line in open('16.in').readlines())
-
     return next(i for i, sue in enumerate(sues) if sue <= real_sue) + 1
 
 
+def p2():
+    preds = {'children': lambda x: x == 3,
+             'cats': lambda x: x > 7,
+             'samoyeds': lambda x: x == 2,
+             'pomeranians': lambda x: x < 3,
+             'akitas': lambda x: x == 0,
+             'vizslas': lambda x: x == 0,
+             'goldfish': lambda x: x < 5,
+             'trees': lambda x: x > 3,
+             'cars': lambda x: x == 2,
+             'perfumes': lambda x: x == 1}
+    sues = ({m.group(1): int(m.group(2))
+             for m in re.finditer(r' ([a-z]+): (\d+)(,|$)', line)}
+            for line in open('16.in').readlines())
+    return next(i for i, sue in enumerate(sues)
+                if all(preds[k](v) for k, v in sue.items())) + 1
+
 print(p1())  # 213
+print(p2())  # 323
