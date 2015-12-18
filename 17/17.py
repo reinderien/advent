d16a1e0dd00d5683b322a2672ade3c6df372907c
reinderien <gtoombs@lakeheadu.ca>
2015-12-17 21:14:06 -0500

    Hashbangs

--- 17/17.py
+++ 17/17.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 containers = sorted((int(l) for l in open('17.in').readlines()), reverse=True)
 
 
@@ -11,6 +13,7 @@ def recurse_p1(index=0, used=0):
 def dump(ids):
     print(' '.join('%d-%d' % (containers[i], i) for i in ids))
 
+
 def recurse_p2(index=0, used=0, n_containers=0, min_containers=float('inf'), n_found=0, ids=list()):
     if used == 150:
         if min_containers > n_containers:
@@ -22,13 +25,14 @@ def recurse_p2(index=0, used=0, n_containers=0, min_containers=float('inf'), n_f
     if used >= 150 or index >= len(containers): return min_containers, n_found
 
     taken_min_containers, taken_n_found = recurse_p2(
-        index=index+1, min_containers=min_containers, n_found=n_found, n_containers=n_containers+1,
-        used=used + containers[index], ids=ids+[index])
+        index=index + 1, min_containers=min_containers, n_found=n_found, n_containers=n_containers + 1,
+        used=used + containers[index], ids=ids + [index])
     if min_containers >= taken_min_containers:
         min_containers, n_found = taken_min_containers, taken_n_found
     return recurse_p2(
-        index=index+1, min_containers=min_containers, n_found=n_found, n_containers=n_containers,
+        index=index + 1, min_containers=min_containers, n_found=n_found, n_containers=n_containers,
         used=used, ids=ids)
+
 
 print(recurse_p1())  # 4372
 print(recurse_p2())  # 4
