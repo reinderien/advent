fb868211ba07a7da29ffe0eeb102529a0112ff0c
reinderien <gtoombs@lakeheadu.ca>
2016-12-08 22:17:54 -0500

    6.1 working

--- 2016/06/06.py
+++ 2016/06/06.py
@@ -1,2 +1,15 @@
 #!/usr/bin/env python3
 
+from collections import Counter
+
+freqs = None
+
+for line in open('06.in'):
+    if freqs is None:
+        freqs = [Counter() for _ in line]
+    for i, c in enumerate(line):
+        freqs[i][c] += 1
+
+msg = ''.join(f.most_common(1)[0][0] for f in freqs)
+print(msg)  # qrqlznrl - Correct
+
