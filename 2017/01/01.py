f70cf4b5be7f869e85148a345b8fcff313350475
reinderien <gtoombs@lakeheadu.ca>
2017-12-06 17:40:21 -0500

    Start 2017, 1 done

---
+++ 2017/01/01.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+def count(off):
+    return sum(x for i, x in enumerate(digits)
+               if x == digits[(i + off) % len(digits)])
+
+with open('01.in') as f:
+    digits = [ord(c) - ord('0') for c in f.read()]
+print('Part 1:', count(1))  # 1150
+print('Part 2:', count(len(digits)//2))  # 1064
