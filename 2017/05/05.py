9dd847854a8608a3f1633f04b77c1d394358351e
reinderien <gtoombs@lakeheadu.ca>
2017-12-07 13:57:59 -0500

    5. tests pass but answer is wrong

--- 2017/05/05.py
+++ 2017/05/05.py
@@ -3,7 +3,6 @@
 
 def run(offsets, first):
     steps, index = 0, 0
-    index = 0
     while True:
         offset = offsets[index]
         if offset >= 3 and not first:
@@ -15,11 +14,10 @@ def run(offsets, first):
         if not (0 <= index < len(offsets)):
             return steps
 
-example = [0, 3, 0, 1, -3]
-assert(run(example, first=True) == 5)
-assert(run(example, first=False) == 10)
+assert(run([0, 3, 0, 1, -3], first=True) == 5)
+assert(run([0, 3, 0, 1, -3], first=False) == 10)
 
 with open('05.in') as f:
     file_offsets = [int(line) for line in f]
-print('Part 1:', run(file_offsets, first=True))  # 376976
-print('Part 2:', run(file_offsets, first=False))  #
+print('Part 1:', run(file_offsets, first=True))   # 376976
+print('Part 2:', run(file_offsets, first=False))  # 739 (wrong)
