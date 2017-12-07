f0b6fdc62730608e1332f146946a182e8ef13f69
reinderien <gtoombs@lakeheadu.ca>
2017-12-07 13:40:09 -0500

    5.1 working, 5.2 does not pass test

--- 2017/05/05.py
+++ 2017/05/05.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 
+
+def run(offsets, first):
+    steps, index = 0, 0
+    index = 0
+    while True:
+        offset = offsets[index]
+        if offset >= 3 and not first:
+            offsets[index] -= 1
+        else:
+            offsets[index] += 1
+        index += offset
+        steps += 1
+        if not (0 <= index < len(offsets)):
+            return steps
+
+example = [0, 3, 0, 1, -3]
+assert(run(example, first=True) == 5)
+assert(run(example, first=False) == 10)
+
 with open('05.in') as f:
-    offsets = tuple(int(line) for line in f)
+    file_offsets = [int(line) for line in f]
+print('Part 1:', run(file_offsets, first=True))  # 376976
+print('Part 2:', run(file_offsets, first=False))  #
