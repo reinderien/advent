0e5b45a55822661d1ce994f11c4cc28c9808125b
reinderien <gtoombs@lakeheadu.ca>
2017-12-07 13:10:33 -0500

    04.2 working

--- 2017/04/04.py
+++ 2017/04/04.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
-total = 0
+total_1, total_2 = 0, 0
 with open('04.in') as f:
     for line in f:
         words = line.split()
-        total += len(words) == len(set(words))
-print('Part 1:', total)  # 455
-
+        total_1 += len(words) == len(set(words))
+        awords = set(''.join(sorted(w)) for w in words)
+        total_2 += len(awords) == len(words)
+print('Part 1:', total_1)  # 455
+print('Part 2:', total_2)  # 186
