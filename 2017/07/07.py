99a5c6d222908948d4aa39894f72afea1773d606
reinderien <gtoombs@lakeheadu.ca>
2017-12-07 17:17:18 -0500

    7.2 works but wow, is it ugly

--- 2017/07/07.py
+++ 2017/07/07.py
@@ -42,6 +42,30 @@ def parse_tree(raw):
 def top(t):
     return tuple(t.keys())
 
+
+def check_tree(tree):
+    if not tree:
+        return 0
+    weights = tuple((node.weight + check_tree(node.children), node)
+                     for node in tree.values())
+    weight_qs = tuple(w[0] for w in weights)
+    all_weights = set(weight_qs)
+    if len(all_weights) != 1:
+        weight_counts = tuple(sorted((sum(1 for wc in weights if w==wc[0]), w)
+                                     for w in all_weights))
+        bad_weight = weight_counts[0][1]
+        others_weight = weight_counts[1][1]
+        bad_node = next(w[1] for w in weights if w[0] == bad_weight)
+
+        print('Bad node found.')
+        print('                     Name:', bad_node.name)
+        print('Neighbour combined weight:', others_weight)
+        print('      Own combined weight:', bad_weight)
+        print('               Own weight:', bad_node.weight)
+        print('    Own weight to balance:', others_weight - bad_weight + bad_node.weight)
+        raise EOFError()
+    return sum(weight_qs)
+
 test_input = '''pbga (66)
 xhth (57)
 ebii (61)
@@ -63,3 +87,31 @@ with open('07.in') as f:
 
 assert(top(test_tree) == ('tknk',))
 print('Part 1:', top(file_tree))  # mwzaxaj
+print()
+
+print('Part 2 test:')
+try:
+    check_tree(test_tree)
+except EOFError:
+    pass
+'''
+                     Name: ugml
+Neighbour combined weight: 243
+      Own combined weight: 251
+               Own weight: 68
+    Own weight to balance: 60
+'''
+
+print()
+print('Part 2:')
+try:
+    check_tree(file_tree)
+except EOFError:
+    pass
+'''
+                     Name: vrgxe
+Neighbour combined weight: 2159
+      Own combined weight: 2166
+               Own weight: 1226
+    Own weight to balance: 1219
+'''
