882fe0248a13da18d79960d5e8109d7c52c5710a
reinderien <gtoombs@lakeheadu.ca>
2017-12-08 11:36:31 -0500

    2016 7.2 working, finally

--- 2016/07/07.py
+++ 2016/07/07.py
@@ -22,19 +22,29 @@ def p1():
     assert(valid('ioxxoj[abbbbe]zxcvbn'))
 
     nvalid = sum(1 for line in open('07.in') if valid(line))
-    print(nvalid)
+    print(nvalid)  # 115
 
 
 def p2():
-    first = r'(\w)(?!\1)(\w)\1'
-    others = r'(\w*\[\w*\]\w*)*'
-    second = r'\2\1\2'
-    openb = r'\[\w*'
-    closeb = r'\w*\]'
-    patl = re.compile(first + others + openb + second + closeb)
-    patr = re.compile(openb + first + closeb + others + second)
+    rex_aba = re.compile(r'(\w)(?!\1)(\w)\1')
+    rex_inner = re.compile(r'\[(.+?)\]')
+    rex_outer = re.compile(r'[^\[\]]+(?=\[|$)')
+
     def valid(ip):
-        return any(p.search(ip) for p in (patl, patr))
+        outer = tuple(m[0] for m in rex_outer.finditer(ip))
+
+        for inner in rex_inner.finditer(ip):
+            haystack = inner[1]
+            while True:  # Loop for overlapping matches
+                aba_inner = rex_aba.search(haystack)
+                if not aba_inner:
+                    break
+                bab = aba_inner.expand(r'\2\1\2')
+                if any(bab in o for o in outer):
+                    return True
+                haystack = haystack[aba_inner.start(0)+1:]
+
+        return False
 
     # Tests from question
     assert(    valid('aba[bab]xyz'))
@@ -43,6 +53,7 @@ def p2():
     assert(    valid('zazbz[bzb]cdb'))
 
     # Addl tests
+    assert(not valid('sss[sss]sss'))
     assert(not valid('[aba]xx[bab]'))
     assert(    valid('abaxx[1]2[3]4[5bab6]'))
     assert(    valid('3[5bab6][1]2[3]4aba'))
@@ -50,4 +61,4 @@ def p2():
     nvalid = sum(1 for line in open('07.in') if valid(line))
     print(nvalid)
 
-p2()  # 39, 82, 153 too low
+p2()  # 231
