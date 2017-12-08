ca4d05b7679d7885c2f23488c6275259521a47a2
reinderien <gtoombs@lakeheadu.ca>
2017-12-08 16:47:37 -0500

    11 started

--- 2016/11/11.py
+++ 2016/11/11.py
@@ -1,2 +1,22 @@
 #!/usr/bin/env python3
 
+'''
+Positive generator, negative chip.
+By atomic number,
+    38 Strontium
+    44 Ruthenium
+    61 Promethium
+    69 Thulium
+    94 Plutonium
+'''
+
+floors = (
+    { 69, -69, 94, 38},
+    {-94, -38},
+    { 61, -61, 44, -44},
+    set()
+)
+
+objects = {o: i for i, objs in enumerate(floors)
+           for o in objs}
+
