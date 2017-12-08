bbc5cdfadc3f6030acc4ab31bcae2094659d29ca
reinderien <gtoombs@lakeheadu.ca>
2017-12-08 12:32:04 -0500

    9.2 working

--- 2016/09/09.py
+++ 2016/09/09.py
@@ -2,10 +2,11 @@
 
 import re
 
+marker = re.compile(r'\((\d+)x(\d+)\)')
+
 
 def dec(line):
     dec_len, zip_i = 0, 0
-    marker = re.compile(r'\((\d+)x(\d+)\)')
 
     while True:
         match = marker.search(line, zip_i)
@@ -19,7 +20,19 @@ def dec(line):
 
 
 def dec2(line):
-    pass
+    def recurse(i_start, i_end):
+        dec_len = 0
+        while True:
+            match = marker.search(line, i_start, i_end)
+            if not match:
+                return dec_len + i_end - i_start
+            span, rep = (int(g) for g in match.groups())
+            i_new_start = match.end() + span
+            dec_len += match.start() - i_start + rep*recurse(i_start=match.end(),
+                                                             i_end=i_new_start)
+            i_start = i_new_start
+    return recurse(0, len(line))
+
 
 with open('09.in') as f:
     file_line = f.read().strip()
@@ -36,4 +49,4 @@ assert(dec2('(3x3)XYZ') == 9)
 assert(dec2('X(8x2)(3x3)ABCY') == 20)
 assert(dec2('(27x12)(20x12)(13x14)(7x10)(1x12)A') == 241920)
 assert(dec2('(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN') == 445)
-print('Part 2:', dec2(file_line))  #
+print('Part 2:', dec2(file_line))  # 11797310782
