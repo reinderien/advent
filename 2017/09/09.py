f6b606d7f140981150f17e7890917d50c04c21db
reinderien <gtoombs@lakeheadu.ca>
2017-12-10 23:25:55 -0500

    9.2 working

--- 2017/09/09.py
+++ 2017/09/09.py
@@ -11,7 +11,7 @@ def find_any(haystack, needles, start=0):
 
 
 def run(text):
-    score, index, n_groups, level = 0, 0, 0, 0
+    score, index, n_groups, level, n_garbage, garbage_start = (0,)*6
     group_next = set('{}<')
     garbage_next = set('!>')
     in_garbage = False
@@ -20,13 +20,15 @@ def run(text):
         searching_for = garbage_next if in_garbage else group_next
         next_index, next_char = find_any(text, searching_for, index)
         if next_index is None:
-            return n_groups, score
+            return n_groups, score, n_garbage
         index = next_index + 1
         if in_garbage:
             if next_char == '!':
                 index += 1
+                garbage_start += 2
             elif next_char == '>':
                 in_garbage = False
+                n_garbage += index - garbage_start - 1
         else:
             if next_char == '{':
                 n_groups += 1
@@ -36,41 +38,42 @@ def run(text):
                 level -= 1
             elif next_char == '<':
                 in_garbage = True
+                garbage_start = index
 
 
-test_n_groups, _ = run('{}')
+test_n_groups, _, _ = run('{}')
 assert(test_n_groups == 1)
-test_n_groups, _ = run('{{{}}}')
+test_n_groups, _, _ = run('{{{}}}')
 assert(test_n_groups == 3)
-test_n_groups, _ = run('{{},{}}')
+test_n_groups, _, _ = run('{{},{}}')
 assert(test_n_groups == 3)
-test_n_groups, _ = run('{{{},{},{{}}}}')
+test_n_groups, _, _ = run('{{{},{},{{}}}}')
 assert(test_n_groups == 6)
-test_n_groups, _ = run('{<{},{},{{}}>}')
+test_n_groups, _, _ = run('{<{},{},{{}}>}')
 assert(test_n_groups == 1)
-test_n_groups, _ = run('{<a>,<a>,<a>,<a>}')
+test_n_groups, _, _ = run('{<a>,<a>,<a>,<a>}')
 assert(test_n_groups == 1)
-test_n_groups, _ = run('{{<a>},{<a>},{<a>},{<a>}}')
+test_n_groups, _, _ = run('{{<a>},{<a>},{<a>},{<a>}}')
 assert(test_n_groups == 5)
-test_n_groups, _ = run('{{<!>},{<!>},{<!>},{<a>}}')
+test_n_groups, _, _ = run('{{<!>},{<!>},{<!>},{<a>}}')
 assert(test_n_groups == 2)
 
-_, test_score = run('{}')
+_, test_score, _ = run('{}')
 assert(test_score == 1)
-_, test_score = run('{{{}}}')
+_, test_score, _ = run('{{{}}}')
 assert(test_score == 6)
-_, test_score = run('{{},{}}')
+_, test_score, _ = run('{{},{}}')
 assert(test_score == 5)
-_, test_score = run('{{{},{},{{}}}}')
+_, test_score, _ = run('{{{},{},{{}}}}')
 assert(test_score == 16)
-_, test_score = run('{<a>,<a>,<a>,<a>}')
+_, test_score, _ = run('{<a>,<a>,<a>,<a>}')
 assert(test_score == 1)
-_, test_score = run('{{<ab>},{<ab>},{<ab>},{<ab>}}')
+_, test_score, _ = run('{{<ab>},{<ab>},{<ab>},{<ab>}}')
 assert(test_score == 9)
-_, test_score = run('{{<!!>},{<!!>},{<!!>},{<!!>}}')
+_, test_score, _ = run('{{<!!>},{<!!>},{<!!>},{<!!>}}')
 assert(test_score == 9)
-_, test_score = run('{{<a!>},{<a!>},{<a!>},{<ab>}}')
+_, test_score, _ = run('{{<a!>},{<a!>},{<a!>},{<ab>}}')
 assert(test_score == 3)
 
 with open('09.in') as f:
-    print('Part 1: %d groups, %d score' % run(f.read()))  # 12505
+    print('%d groups, %d score, %d garbage' % run(f.read()))  # P1: 12505  P2: 6671
