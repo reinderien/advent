eaaebd1b46123235ae30dcc60d88ea03ec6af7cb
reinderien <gtoombs@lakeheadu.ca>
2017-12-12 22:15:44 -0500

    13.1, 2 working

--- 2016/13/13.py
+++ 2016/13/13.py
@@ -12,8 +12,9 @@ def ones(x):
     return n
 
 
-def run(fav, goal):
+def run(fav, goal=None):
     def is_wall(x, y):
+        if x<0 or y<0: return True
         n = ones(x*(x + 3) + y*(y + 2*x + 1) + fav)
         return n & 1
 
@@ -27,14 +28,28 @@ def run(fav, goal):
             return 1
 
         def neighbors(self, node):
-            x0, y0 = node
-            neighs = tuple((x0+x, y0+y) for (x,y) in
+            x, y = node
+            neighs = tuple((x+a, y+b) for a,b in
                            ((1,0), (-1,0), (0,1), (0,-1)))
             return (n for n in neighs if not is_wall(*n))
 
-    path = tuple(Cubicles().astar(start=(1,1), goal=goal))
-    return len(path) - 1
+    cubes = Cubicles()
+    if goal:
+        path = tuple(cubes.astar(start=(1,1), goal=goal))
+        return len(path) - 1
+
+    n_locs = 0
+    for gy in range(52):
+        for gx in range(int(sqrt(52**2 - gy**2))):
+            if is_wall(gx, gy):
+                continue
+            path = cubes.astar(start=(1,1), goal=(gx,gy))
+            if path is not None and len(tuple(path))-1 <= 50:
+                n_locs += 1
+    return n_locs
 
 
 assert(run(10, (7, 4)) == 11)
-print('Part 1:', run(1362, (31, 39)))  # 82
+real_fav, real_goal = 1362, (31, 39)
+print('Part 1:', run(real_fav, real_goal))  # 82
+print('Part 2:', run(real_fav))  # 138 (slow)
