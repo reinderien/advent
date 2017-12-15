e8be728f3f62bcacf89516866424be4c094d4564
reinderien <gtoombs@lakeheadu.ca>
2017-12-15 11:12:38 -0500

    12.2 done

--- 2017/12/12.py
+++ 2017/12/12.py
@@ -23,7 +23,16 @@ def run(lines):
         return len(visited)
 
     def count_all():
-        return 2
+        n_groups = 0
+        while direct:
+            visited, pending = set(), {next(iter(direct.keys()))}
+            while pending:
+                current = pending.pop()
+                visited.add(current)
+                pending |= direct[current] - visited
+                direct.pop(current)
+            n_groups += 1
+        return n_groups
 
     parse()
     return count_0(), count_all()
@@ -41,4 +50,4 @@ assert(run(test_input) == (6, 2))  # works
 with open('12.in') as f:
     count_0_real, count_all_real = run(f)
 print('Part 1:', count_0_real)  # 141
-print('Part 2:', count_all_real)  #
+print('Part 2:', count_all_real)  # 171
