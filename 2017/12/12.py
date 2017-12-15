2fdc06c9d63f0afd92f2096e9091b46018b8e968
reinderien <gtoombs@lakeheadu.ca>
2017-12-15 10:46:51 -0500

    12.1 foxed by walking the network

--- 2017/12/12.py
+++ 2017/12/12.py
@@ -1,39 +1,29 @@
 #!/usr/bin/env python3
 
 import re
-from itertools import combinations
 
 
 def run(lines):
-    groups = {}
-
-    def assoc(i, j):
-        i_group, j_group = groups.get(i), groups.get(j)
-        if i_group is None and j_group is None:
-            group = {i, j}
-            groups[i] = group
-            groups[j] = group
-        elif i_group is not None and j_group is None:
-            i_group.add(j)
-            groups[j] = i_group
-        elif j_group is not None and i_group is None:
-            j_group.add(i)
-            groups[i] = j_group
-        elif i_group is not None and j_group is not None:
-            i_group |= j_group
-            groups[j] = i_group
+    direct = {}
 
     def parse():
         rex = re.compile(r'\d+')
         for line in lines:
             ids = tuple(int(i) for i in rex.findall(line))
-            for i1, i2 in combinations(ids, 2):
-                assoc(i1, i2)
-            # assoc(ids[0], ids[-1])
+            for i, j in zip(ids[0:], ids[1:]):
+                direct.setdefault(i, set()).add(j)
+                direct.setdefault(j, set()).add(i)
+
+    def count():
+        visited, pending = set(), {0}
+        while pending:
+            current = pending.pop()
+            visited.add(current)
+            pending |= direct[current] - visited
+        return len(visited)
 
     parse()
-
-    return len(groups[0])
+    return count()
 
 
 test_input = '''0 <-> 2
@@ -45,9 +35,5 @@ test_input = '''0 <-> 2
 6 <-> 4, 5'''.split('\n')
 assert(run(test_input) == 6)  # works
 
-'''
-21 is wrong
-4 is wrong
-'''
 with open('12.in') as f:
-    print('Part 1:', run(f))
+    print('Part 1:', run(f))  # 141
