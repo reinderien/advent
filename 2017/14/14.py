5dc86d6df7ea8e83405dbc7aa09f24db5f730f11
reinderien <gtoombs@lakeheadu.ca>
2017-12-15 22:31:02 -0500

    Prepare for 14.2

--- 2017/14/14.py
+++ 2017/14/14.py
@@ -19,15 +19,28 @@ def knot(byte_string):
     return bytearray(reduce(xor, hash_seq[i:i+16]) for i in range(0, 256, 16))
 
 
-def run(key):
+def get_hashes(key):
+    return tuple(knot('%s-%d' % (key, row))
+                 for row in range(128))
+
+
+def p1(hashes):
     used = 0
-    for row in range(128):
-        for byte in knot('%s-%d' % (key, row)):
+    for row in hashes:
+        for byte in row:
             while byte:
                 if byte & 1:
                     used += 1
                 byte >>= 1
     return used
 
-assert(run('flqrgnkx') == 8108)
-print('Part 1:', run('hwlqcszp'))  # 8304
+
+def p2(hashes):
+    pass
+
+test_hashes = get_hashes('flqrgnkx')
+real_hashes = get_hashes('hwlqcszp')
+assert(p1(test_hashes) == 8108)
+print('Part 1:', p1(real_hashes))  # 8304
+assert(p2(test_hashes) == 1242)
+print('Part 2:', p2(real_hashes))  #
