447a761bd60e3ebefa43780bfc1dcd04a4945d98
reinderien <gtoombs@lakeheadu.ca>
2017-12-21 00:04:41 -0500

    Slightly faster 13

--- 2017/13/13.py
+++ 2017/13/13.py
@@ -8,22 +8,23 @@ def run(fname):
         scanners = tuple(tuple(int(i) for i in line.rstrip().split(': '))
                          for line in f)
 
-    def get_sev(part2=False, delay=0):
+    def get_sev(delay=0):
         sev = 0
         for t, s_range in scanners:
             r = s_range-1
             scanner = r - abs(r - (t+delay) % (2*r))
             if scanner == 0:
-                if part2:
-                    return True
                 sev += s_range*t
-        if part2:
-            return False
         return sev
 
     def dodge():
         for delay in count():
-            if not get_sev(True, delay):
+            for t, s_range in scanners:
+                r = s_range-1
+                scanner = r - abs(r - (t+delay) % (2*r))
+                if scanner == 0:
+                    break
+            else:
                 return delay
 
     return get_sev(), dodge()
