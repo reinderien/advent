c506cf3d787b642fe931c95fd3aa0f23f6caffe3
reinderien <gtoombs@lakeheadu.ca>
2018-01-02 15:22:55 -0500

    Only show labels that are used

--- 2017/23/23_2.py
+++ 2017/23/23_2.py
@@ -1,18 +1,31 @@
 #!/usr/bin/env python3
 
-with open('23.in', 'r') as fin, \
-    open('prog.c', 'w') as fout:
+gotos = set()
+lines = []
 
+with open('23.in', 'r') as fin:
     for lno, line in enumerate(fin):
-        fout.write('l%02d:\t' % lno)
         op, x, y = line.rstrip().split(' ')
+        oline = '\t'
         if op == 'set':
-            fout.write('%s = %s' % (x, y))
+            oline += '%s = %s' % (x, y)
         elif op == 'mul':
-            fout.write('%s *= %s' % (x, y))
+            oline += '%s *= %s' % (x, y)
         elif op == 'sub':
-            fout.write('%s -= %s' % (x, y))
+            oline += '%s -= %s' % (x, y)
         elif op == 'jnz':
-            fout.write('if (%s != 0) goto l%02d' % (x, lno+int(y)))
-        fout.write(';\t// %s' % line)
+            goto = lno+int(y)
+            gotos.add(goto)
+            oline += 'if (%s != 0) goto l%02d' % (x, goto)
+        else:
+            raise ValueError()
+        oline += ';\t// %s' % line
+        lines.append(oline)
+
+with open('prog.c', 'w') as fout:
+    for lno, line in enumerate(lines):
+        if lno in gotos:
+            fout.write('l%02d:%s' % (lno, line))
+        else:
+            fout.write('\t%s' % line)
     fout.write('l%02d: ;\n' % (lno+1))
