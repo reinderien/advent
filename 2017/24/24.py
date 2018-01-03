86c2ffe40e28061f92e2a1339348f64a6301418e
reinderien <gtoombs@lakeheadu.ca>
2018-01-03 15:45:39 -0500

    Rearrange into parse before p2

--- 2017/24/24.py
+++ 2017/24/24.py
@@ -3,7 +3,7 @@
 from collections import defaultdict
 
 
-def run(fname):
+def parse(fname):
     # Lines ("parts") are unique regardless of orientation.
     graph = defaultdict(set)
     with open(fname) as f:
@@ -11,14 +11,22 @@ def run(fname):
             a, b = (int(n) for n in line.split('/'))
             graph[a].add(b)
             graph[b].add(a)
+    return graph
 
-    def recurse(visited, left=0):
-        next_pairs = {(left, right) for right in graph[left]} - visited
-        if not next_pairs:
-            return 0
-        return left + max(pair[1] + recurse(visited | {pair, pair[::-1]}, pair[1])
-                          for pair in next_pairs)
-    return recurse(set())
 
-assert(run('24.test.in') == 31)
-print('Part 1:', run('24.in'))  # 1859
+def recurse_p1(graph, visited, left=0):
+    next_pairs = {(left, right) for right in graph[left]} - visited
+    if not next_pairs:
+        return 0
+    return left + max(pair[1] + recurse_p1(graph, visited | {pair, pair[::-1]}, pair[1])
+                      for pair in next_pairs)
+
+
+def p1(graph):
+    return recurse_p1(graph, set())
+
+test_graph = parse('24.test.in')
+assert(p1(test_graph) == 31)
+
+real_graph = parse('24.in')
+print('Part 1:', p1(real_graph))  # 1859
