e0e3f5f408341af5e0d4209c71cca3199678f724
greg_toombs <greg.toombs@oracle.com>
2018-11-28 00:34:11 +0700

    P1 works, P2 does not - refactor to use arbitrary word size

--- 2016/16/16.c
+++ 2016/16/16.c
@@ -1,27 +1,34 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
 
-#define DISK_BITS 272 // our input; use 20 to test
-#define DISK_QUADS (DISK_BITS/64 + 1)
+
+// 20 to test
+// 272 for part 1
+// 35651584 for part 2
+#define DISK_BITS 272
+
+typedef uint64_t Word;
+#define WORD_BITS (8*sizeof(Word))
+#define DISK_WORDS (DISK_BITS/WORD_BITS + 1)
 
 
 typedef struct
 {
     unsigned len;
-    uint64_t data[DISK_QUADS];
+    Word data[DISK_WORDS];
 } Fill;
 
 static void init_fill(Fill *restrict f, const char *restrict input)
 {
-    // memfill(f->data, 0, DISK_QUADS*sizeof(uint64_t));
-    for (int d = 0; d < DISK_QUADS; d++)
-        f->data[d] = 0;
+    memset(f->data, 0, DISK_WORDS*sizeof(Word));
 
     const char *c = input;
-    for (uint64_t *data = f->data;; data++)
+    for (Word *data = f->data;; data++)
     {
-        for (uint64_t b = 0; b < 64; b++)
+        for (Word b = 0; b < WORD_BITS; b++)
         {
             if (!*c)
                 return;
@@ -39,18 +46,18 @@ static void fill_churn(Fill *restrict f)
     if (tail_end > DISK_BITS-1)
         b_tail = DISK_BITS-1;
     else b_tail = tail_end;
-    int o_tail = b_tail % 64;                // Offset in tail quad
-    uint64_t *d_tail = f->data + b_tail/64;  // Tail data pointer
+    int o_tail = b_tail % WORD_BITS;            // Offset in tail quad
+    Word *d_tail = f->data + b_tail/WORD_BITS;  // Tail data pointer
 
     f->len = b_tail+1;
 
-    unsigned b_head = tail_end - b_tail;           // Offset of head
-    uint64_t m_head = 1 << (b_head % 64);          // Mask in head quad
-    const uint64_t *d_head = f->data + b_head/64;  // Head data pointer
+    unsigned b_head = tail_end - b_tail;              // Offset of head
+    Word m_head = 1 << (b_head % WORD_BITS);          // Mask in head quad
+    const Word *d_head = f->data + b_head/WORD_BITS;  // Head data pointer
 
     for (; b_head < b_tail; b_head++, b_tail--)
     {
-        *d_tail |= ((uint64_t)!(*d_head & m_head)) << o_tail;
+        *d_tail |= ((Word)!(*d_head & m_head)) << o_tail;
 
         m_head <<= 1;
         if (!m_head)
@@ -62,13 +69,13 @@ static void fill_churn(Fill *restrict f)
         o_tail--;
         if (o_tail < 0)
         {
-            o_tail = 63;
+            o_tail = WORD_BITS-1;
             d_tail--;
         }
     }
 }
 
-static void pretty_bin(const uint64_t *restrict data, unsigned len)
+static void pretty_bin(const Word *restrict data, unsigned len, bool sep)
 {
     unsigned btot = 0;
     for (const uint8_t *d = (const uint8_t*)data;; d++)
@@ -76,37 +83,37 @@ static void pretty_bin(const uint64_t *restrict data, unsigned len)
         for (unsigned b = 0; b < 8; b++)
         {
             putchar('0' + ((*d >> b)&1));
-            if (b==3) putchar('_');
+            if (sep && b==3) putchar('_');
             if (++btot >= len)
             {
                 putchar('\n');
                 return;
             }
         }
-        putchar(' ');
+        if (sep) putchar(' ');
     }
 }
 
 static void checksum(const Fill *restrict f)
 {
-    uint64_t cs[DISK_QUADS/2 + 1];
+    Word cs[DISK_WORDS/2 + 1];
     unsigned in_len = f->len, out_len;
-    const uint64_t *in_dat = f->data;
+    const Word *in_dat = f->data;
 
     do
     {
-        const uint64_t *din = in_dat;
+        const Word *din = in_dat;
         unsigned bin = 0, bin_total = 0;
-        for (uint64_t *dout = cs; bin_total < in_len; dout++)
+        for (Word *dout = cs; bin_total < in_len; dout++)
         {
-            uint64_t new_dout = 0;
-            for (unsigned bout = 0; bout < 64; bout++)
+            Word new_dout = 0;
+            for (unsigned bout = 0; bout < WORD_BITS; bout++)
             {
-                uint64_t bit = !((
-                                     (*din >> bin) & 1
-                                 ) ^ (
-                                     (*din >> (bin+1)) & 1
-                                 ));
+                Word bit = !((
+                                 (*din >> bin) & 1
+                             ) ^ (
+                                 (*din >> (bin+1)) & 1
+                             ));
                 new_dout |= bit << bout;
 
                 bin_total += 2;
@@ -114,7 +121,7 @@ static void checksum(const Fill *restrict f)
                     break;
 
                 bin += 2;
-                if (bin >= 64)
+                if (bin >= WORD_BITS)
                 {
                     bin = 0;
                     din++;
@@ -123,13 +130,15 @@ static void checksum(const Fill *restrict f)
             *dout = new_dout;
         }
         out_len = in_len/2;
-        printf("CS: ");
-        pretty_bin(cs, out_len);
-        printf("Len: %u\n\n", out_len);
 
         in_dat = cs;
         in_len = out_len;
     } while (!(out_len & 1));
+
+
+    printf("CS: ");
+    pretty_bin(cs, out_len, false);
+    printf("CS bits: %u\n\n", out_len);
 }
 
 int main()
@@ -138,22 +147,23 @@ int main()
     const char *input = "10001001100000001"; // our input; use "10000" to test
     init_fill(&fill, input);
 
+    printf("Word bits: %lu\n", WORD_BITS);
     printf("Disk bits: %u\n", DISK_BITS);
-    printf("Disk quads: %u\n", DISK_QUADS);
+    printf("Disk words: %lu\n", DISK_WORDS);
     printf("Initial data: ");
-    pretty_bin(fill.data, fill.len);
-    printf("Initial len: %u\n\n", fill.len);
+    pretty_bin(fill.data, fill.len, true);
+    printf("Initial bits: %u\n\n", fill.len);
 
     while (fill.len < DISK_BITS)
-    {
         fill_churn(&fill);
 
-        printf("Data: ");
-        pretty_bin(fill.data, fill.len);
-        printf("Len: %u\n\n", fill.len);
-    }
+    // printf("Data: ");
+    // pretty_bin(fill.data, fill.len, TRUE);
+    printf("Data bits: %u\n\n", fill.len);
 
     checksum(&fill);
+    // Part 1: 10101001010100001 is correct
+    // Part 2: 10100001101101110 is too low
 
     return 0;
 }
