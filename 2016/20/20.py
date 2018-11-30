5a0e8e0da28b20289aa3a128f531db0329ee8275
greg_toombs <greg.toombs@oracle.com>
2018-11-30 07:30:45 +0700

    Cleanup; p1 works

--- 2016/20/20.py
+++ 2016/20/20.py
@@ -3,7 +3,8 @@
 from bisect import bisect_left
 
 
-def run(fname):
+def run(title, fname, expected=None):
+    print(title + ':')
     starts, ends = None, None
 
     with open(fname) as f:
@@ -24,13 +25,14 @@ def run(fname):
             # all starts[:s1] < ip1
             # all starts[s1:] >= ip1
             s1 = bisect_left(starts, ip1)
+            iter_from = max(0, s1-1)
 
             to_remove = []
             new_s, new_e = ip1, ip2
             insert_at = None
 
             for i, (s, e) in enumerate(
-                    zip(starts[s1:], ends[s1:]), s1):
+                    zip(starts[iter_from:], ends[iter_from:]), iter_from):
                 if s > ip2+1:
                     break
                 if e < ip1-1:
@@ -38,7 +40,7 @@ def run(fname):
                 # Now we need to merge
                 if insert_at is None:
                     insert_at = i
-                print("Merging %u-%u and %u-%u" % (s, e, new_s, new_e))
+                # print("Merging %u-%u and %u-%u" % (s, e, new_s, new_e))
                 to_remove.append(i)
                 new_s = min(new_s, s)
                 new_e = max(new_e, e)
@@ -52,11 +54,17 @@ def run(fname):
 
             starts.insert(insert_at, new_s)
             ends.insert(insert_at, new_e)
+            # assert(starts == list(sorted(starts)))
+            # assert(ends == list(sorted(ends)))
 
     if starts[0] > 0:
-        return 0
-    return ends[0] + 1
+        result = 0
+    else:
+        result = ends[0] + 1
+    print(result)
+    if expected is not None:
+        assert(result == expected)
 
 
-assert(run('test_input.txt') == 3)
-print(run('input.txt'))  # 166476 is too low
+run('Test', 'test_input.txt', 3)
+run('Part 1', 'input.txt', 23923783)
