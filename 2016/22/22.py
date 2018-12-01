a125b5359e0dac84a0472e065f7a984eb98c4d00
greg_toombs <greg.toombs@oracle.com>
2018-12-02 00:45:14 +0400

    22p1 written, no idea if correct

---
+++ 2016/22/22.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+from collections import namedtuple
+import re
+
+
+Node = namedtuple('Node', ('x', 'y', 'size', 'used', 'avail', 'use'))
+
+
+def parse():
+    line_re = re.compile(r'^/dev/grid/node-x(\d+)-y(\d+) +'
+                         r'(\d+)T +'
+                         r'(\d+)T +'
+                         r'(\d+)T +'
+                         r'(\d+)%$')
+
+    with open('input.txt') as f:
+        for line in f:
+            m = line_re.match(line.rstrip())
+            if not m:
+                continue
+            yield Node(*(int(i) for i in m.groups()))
+
+
+def p1():
+    nodes = list(parse())
+    # data on A fits on B
+    nodes.sort(key=lambda n: n.avail)
+
+    total = 0
+    to_idx = 0
+    for from_idx in range(len(nodes) - 1):
+        while nodes[from_idx].used > nodes[to_idx].avail and \
+         from_idx < len(nodes)-1:
+            from_idx += 1
+
+        total += len(nodes) - to_idx + 1
+
+    return total
+
+
+print(p1())
